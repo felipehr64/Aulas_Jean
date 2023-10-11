3ef7614c70f0ff1ca78f4f15a501ff4f7fa4f04f
felipehr64 <145224518+felipehr64@users.noreply.github.com>
2023-10-10 22:14:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function resultado(){
+ nome = document.getElementById("name").value;
+ email = document.getElementById("email").value;
+ document.getElementById("resultado").innerHTML = "Nome do Aluno:" + nome + "<br> email" + email;
+}
