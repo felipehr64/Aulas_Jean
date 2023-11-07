798066035d96163e9b4e5a9e8d3b2a5f7b032902
felipehr64 <felipehr01@gmail.com>
2023-11-07 20:49:54 -0300

    lol

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function resultado(){
- nome = document.getElementById("name").value;
- email = document.getElementById("email").value;
- document.getElementById("resultado").innerHTML = "Nome do Aluno:" + nome + "<br> email" + email;
-}
+var card = '<div class="card" style="width:400px"><img class="card-img-top" src="img_avatar1.png" alt="Card image"><div class="card-body">  <h4 class="card-title">John Doe</h4> <p class="card-text">Some example text.</p> <a href="#" class="btn btn-primary">See Profile</a> </div> </div>';
+
+var vardadiv = document.getElementById("conteudo");
+
+vardadiv.innerHTML = card + card + card + card;
